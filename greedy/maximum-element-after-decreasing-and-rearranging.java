a49e8291344f2776d6d4b259476b632f6f8b807f
leetudit-hue <leetudit@gmail.com>
2026-06-28 11:34:24 +0530

    LC-maximum-element-after-decreasing-and-rearranging [Runtime: N/A; Memory: N/A]

---
+++ greedy/maximum-element-after-decreasing-and-rearranging.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        if(arr[0]!=1){
+            arr[0] = 1;
+        }
+        for(int i = 1 ; i < n ; i++){
+            if(arr[i]-arr[i-1] >= 1){
+                arr[i] = arr[i-1]+1;
+            }
+        }
+        return arr[n-1];
+    }
+}
