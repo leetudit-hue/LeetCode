8fc8101fc861cef40e4160ff6c3937232de31281
leetudit-hue <leetudit@gmail.com>
2026-06-28 13:17:03 +0530

    LC-merge-intervals [Runtime: N/A; Memory: N/A]

--- sorting/merge-intervals.java
+++ sorting/merge-intervals.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         int n = intervals.length;
-        List<int[]>ans = new ArrayList<>();
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        ArrayList<int[]>ans = new ArrayList<>();
         int st = intervals[0][0];
         int end = intervals[0][1];
         for(int i = 1 ; i < n ; i++){
-            if(intervals[i][0]<=end){
+            if(intervals[i][0] <= end) {
                 end = Math.max(end,intervals[i][1]);
             }
             else{
                 ans.add(new int[]{st,end});
-                end = intervals[i][1];
                 st = intervals[i][0];
+                end = intervals[i][1];
             }
         }
         ans.add(new int[]{st,end});
-        return ans.toArray(new int[ans.size()][]);
+        return ans.toArray(new int[ans.size()][2]);
     }
 }
