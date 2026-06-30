cecb7843275e9cba53214ac8e6b0dff57794185d
leetudit-hue <leetudit@gmail.com>
2026-06-30 17:55:33 +0530

    LC-number-of-segments-in-a-string [Runtime: N/A; Memory: N/A]

--- string/number-of-segments-in-a-string.java
+++ string/number-of-segments-in-a-string.java
@@ -2,7 +2,7 @@ class Solution {
     public int countSegments(String s) {
         int n = s.length();
         int count = 0;
-        if(s.length()<1) return count;
+        if(n<1 || s.isBlank()) return count;
         for(int i = 0 ; i < n ; i++){
             if(s.charAt(i) == ' '){
                 count++;
