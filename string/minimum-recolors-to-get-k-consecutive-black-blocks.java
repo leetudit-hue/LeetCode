a668f0084a1ac577f67a3a272f5ca7bac8ca799a
leetudit-hue <leetudit@gmail.com>
2026-07-03 12:57:54 +0530

    LC-minimum-recolors-to-get-k-consecutive-black-blocks [Runtime: N/A; Memory: N/A]

---
+++ string/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumRecolors(String blocks, int k) {
+        int n = blocks.length();
+        int i = 0;
+        int ans = Integer.MAX_VALUE;
+        while(i <= n-k){
+            int count = 0;
+            for(int j = i ; j < n ; j++){
+                if(blocks.charAt(j) == 'W'){
+                    count++;
+                }
+            }
+            ans = Math.min(ans,count);
+            i++;
+        }
+        return ans-1;
+    }
+}
