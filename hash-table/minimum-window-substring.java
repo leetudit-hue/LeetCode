da4c37650e26164e9c89770ffc70e86d05c0d21c
leetudit-hue <leetudit@gmail.com>
2026-07-04 11:30:09 +0530

    LC-minimum-window-substring [Runtime: N/A; Memory: N/A]

--- hash-table/minimum-window-substring.java
+++ hash-table/minimum-window-substring.java
@@ -1,32 +1,37 @@
 class Solution {
     public String minWindow(String s, String t) {
         int n = s.length();
-        if(t.length() > n) return "";
         int m = t.length();
-        int start = -1;
-        int minL = Integer.MAX_VALUE;
-        for(int i = 0 ; i < n ; i++){
-            int count = m;
-            HashMap<Character,Integer>map = new HashMap<>();
-            for(int j = 0 ; j < m ; j++){
-                char ch = t.charAt(j);
-                map.put(ch,map.getOrDefault(ch,0)+1);
-            }
-            for(int k = i ; k < n ; k++){
-                char ch = s.charAt(k);
-                if(map.containsKey(ch) && map.get(ch) > 0){
-                    count--;
-                }
-                if(count == 0){
-                    if(k-i+1 < minL){
-                        minL = k-i+1;
-                        start = i;
-                    }
-                    break;
-                }
-                map.put(ch,map.getOrDefault(ch,0)-1);
-            }
+        if(m > n) return "";
+        HashMap<Character,Integer>map = new HashMap<>();
+        for(int i = 0 ; i < m ; i++){
+            char ch = t.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        return minL == Integer.MAX_VALUE ? "" : s.substring(start,start+minL);
+        int i = 0 , j = 0;
+        int st = -1;
+        int minL = Integer.MAX_VALUE;
+        int count = m;
+        while(j < n){
+            char ch = s.charAt(j);
+            if(map.containsKey(ch) && map.get(ch) > 0){
+                count--;
+            }
+            map.put(ch,map.getOrDefault(ch,0)-1);
+            while(count == 0){
+                if(minL > j - i + 1){
+                    minL = j - i + 1;
+                    st = i;
+                }
+                char left = s.charAt(i);
+                map.put(left,map.get(left)+1);
+                if(map.get(left) > 0){
+                    count ++;
+                }
+                i++;
+            }
+            j++;
+        }
+        return st == -1 ? "" : s.substring(st,st+minL);
     }
 }
