0884e165a37a86008a453545adcab78272ca7e52
leetudit-hue <leetudit@gmail.com>
2026-07-04 11:41:09 +0530

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- array/two-sum.java
+++ array/two-sum.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[]ans = new int[2];
-        HashMap<Integer,Integer>mp = new HashMap<>();
         int n = nums.length;
-        for(int i = 0 ; i < n ; i++){
-            int s = target - nums[i];
-            if(mp.containsKey(s)){
-                ans[0] = i;
-                ans[1] = mp.get(s);
-                return ans;
+        int[]ans = new int[2];
+        for(int i = 0 ; i < n-1 ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                if(nums[i] + nums[j] == target){
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
             }
-            mp.put(nums[i],i);
         }
         return ans;
     }
