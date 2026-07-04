6087e52b263528f5662469cc2446c59f40be9437
leetudit-hue <leetudit@gmail.com>
2026-07-04 10:26:55 +0530

    LC-minimum-window-substring [Runtime: N/A; Memory: N/A]

--- hash-table/minimum-window-substring.java
+++ hash-table/minimum-window-substring.java
@@ -1,37 +1,32 @@
 class Solution {
     public String minWindow(String s, String t) {
         int n = s.length();
-        if(n < t.length()) return "";
-        HashMap<Character,Integer>map = new HashMap<>();
-        for(int i = 0 ; i < t.length() ; i++){
-            char ch = t.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
-        int i = 0;
-        int j = 0;
-        int st = 0;
-        int minWindowLength = Integer.MAX_VALUE;
-        int req = t.length();
-        while(j < n){
-            char ch = s.charAt(j);
-            if(map.containsKey(ch) && map.get(ch)>0){
-                req--;
+        if(t.length() > n) return "";
+        int m = t.length();
+        int start = -1;
+        int minL = Integer.MAX_VALUE;
+        for(int i = 0 ; i < n ; i++){
+            int count = m;
+            HashMap<Character,Integer>map = new HashMap<>();
+            for(int j = 0 ; j < m ; j++){
+                char ch = t.charAt(j);
+                map.put(ch,map.getOrDefault(ch,0)+1);
             }
-            map.put(ch,map.getOrDefault(ch,0)-1);
-            while(req == 0){
-                if (j - i + 1 < minWindowLength) {
-                    minWindowLength = j - i + 1;
-                    st = i;
+            for(int k = i ; k < n ; k++){
+                char ch = s.charAt(k);
+                if(map.containsKey(ch) && map.get(ch) > 0){
+                    count--;
                 }
-                char left = s.charAt(i);
-                map.put(left,map.getOrDefault(left,0)+1);
-                if(map.get(left) > 0){
-                    req ++;
+                if(count == 0){
+                    if(k-i+1 < minL){
+                        minL = k-i+1;
+                        start = i;
+                    }
+                    break;
                 }
-                i++;
+                map.put(ch,map.getOrDefault(ch,0)-1);
             }
-            j++;
         }
-        return minWindowLength == Integer.MAX_VALUE ? "" : s.substring(st,minWindowLength+st);
+        return minL == Integer.MAX_VALUE ? "" : s.substring(start,start+minL);
     }
 }
