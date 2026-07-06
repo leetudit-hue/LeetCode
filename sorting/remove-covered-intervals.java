d0850a10104c2927a0a7ddf9d6b0a6f05a1b7168
leetudit-hue <leetudit@gmail.com>
2026-07-06 21:48:42 +0530

    LC-remove-covered-intervals [Runtime: N/A; Memory: N/A]

---
+++ sorting/remove-covered-intervals.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        int n = intervals.length;
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        ArrayList<int[]>arr = new ArrayList<>();
+        arr.add(intervals[0]);
+        for(int i = 0 ; i < n ; i++){
+            int st = arr.get(arr.size()-1)[0];
+            int end = arr.get(arr.size()-1)[1];
+            if(intervals[i][0] >= st && intervals[i][1] <= end){
+                continue;
+            }
+            else{
+                arr.add(intervals[i]);
+            }
+        }
+        return arr.size();
+    }
+}
