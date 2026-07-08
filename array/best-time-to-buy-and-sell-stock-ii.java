dde11c6e9a29267baa4344c6135adfe02c9c86af
leetudit-hue <leetudit@gmail.com>
2026-07-08 22:17:47 +0530

    LC-best-time-to-buy-and-sell-stock-ii [Runtime: N/A; Memory: N/A]

--- array/best-time-to-buy-and-sell-stock-ii.java
+++ array/best-time-to-buy-and-sell-stock-ii.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int profit = 0;
+        int ans = 0;
         for(int i = 1 ; i < n ; i++){
-            if(prices[i]>prices[i-1]){
-                profit+=prices[i]-prices[i-1];
+            if(prices[i] > prices[i-1]){
+                ans += (prices[i] - prices[i-1]);
             }
         }
-        return profit;
+        return ans;
     }
 }
