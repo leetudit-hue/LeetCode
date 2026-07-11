afed3d21ecd2963f9918278f54f06e102dac2a69
leetudit-hue <leetudit@gmail.com>
2026-07-11 22:48:07 +0530

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- array/search-in-rotated-sorted-array.java
+++ array/search-in-rotated-sorted-array.java
@@ -1,73 +1,41 @@
 class Solution {
-    static int binarySearch(int[] arr, int tar, int st, int end) {
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-
-            if (arr[mid] == tar) {
-                return mid;
-            } 
-            else if (arr[mid] > tar) {
-                end = mid - 1;
-            } 
-            else {
-                st = mid + 1;
+    public static int findPvt(int[]nums){
+        int l = 0;
+        int r = nums.length - 1;
+        while(l < r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] > nums[r]){
+                l = mid + 1;
+            }
+            else{
+                r = mid;
             }
         }
-        return -1;
+        return l;
     }
 
-    static int findPvt(int[] arr) {
-        int st = 0;
-        int end = arr.length - 1;
-
-        // array not rotated
-        if (arr[st] < arr[end]) {
-            return -1;
-        }
-
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-
-            // pivot condition
-            if (mid < end && arr[mid] > arr[mid + 1]) {
+    public static int bs(int[]nums,int l,int r,int tar){
+        int mid = -1;
+        int ans = -1;
+        while(l <= r){
+            mid = l + (r - l) / 2;
+            if(nums[mid] > tar){
+                r = mid - 1;
+            }
+            else if(nums[mid] < tar){
+                l = mid + 1;
+            }
+            else{
                 return mid;
             }
-
-            if (mid > st && arr[mid] < arr[mid - 1]) {
-                return mid - 1;
-            }
-
-            // search left/right
-            if (arr[st] <= arr[mid]) {
-                st = mid + 1;
-            } else {
-                end = mid - 1;
-            }
         }
-
-        return -1;
+        return ans;
     }
-
     public int search(int[] nums, int target) {
-
+        int n = nums.length;
         int pvt = findPvt(nums);
-
-        // normal sorted array
-        if (pvt == -1) {
-            return binarySearch(nums, target, 0, nums.length - 1);
-        }
-
-        // target is pivot
-        if (nums[pvt] == target) {
-            return pvt;
-        }
-
-        // left side
-        if (target >= nums[0]) {
-            return binarySearch(nums, target, 0, pvt - 1);
-        }
-
-        // right side
-        return binarySearch(nums, target, pvt + 1, nums.length - 1);
+        int left = bs(nums,0,pvt-1,target);
+        if(left != -1) return left;
+        return bs(nums,pvt,n-1,target);
     }
 }
