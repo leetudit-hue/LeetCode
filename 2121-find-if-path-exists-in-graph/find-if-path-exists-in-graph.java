4b9495748dd0e6082ba9f4efc911fcb3d846ee5d
leetudit-hue <leetudit@gmail.com>
2026-07-18 11:58:29 +0530

    Time: 56 ms (85.44%) | Memory: 282.5 MB (40.95%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,47 +1,32 @@
 class Solution {
-
-    public static boolean bfs(int n, int[][] edges, int st, boolean[] vis, int dest) {
-
+    public static boolean bfs(int n , int[][] adj , int src , int dest , boolean[] vis){
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
+        for(int i = 0 ; i < n ; i++){
             graph.add(new ArrayList<>());
         }
-
-        for (int[] edge : edges) {
+        for(int edge[] : adj){
             int u = edge[0];
             int v = edge[1];
-
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
-
-        Queue<Integer> q = new LinkedList<>();
-        q.add(st);
-        vis[st] = true;
-
-        while (!q.isEmpty()) {
-
-            int curr = q.remove();
-
-            if (curr == dest)
-                return true;
-
-            for (int neighbour : graph.get(curr)) {
-                if (!vis[neighbour]) {
-                    vis[neighbour] = true;
+        Queue<Integer>q = new LinkedList<>();
+        q.add(src);
+        vis[src] = true;
+        while(q.size() > 0){
+            int vertex = q.remove();
+            if(vertex == dest) return true;
+            for(int neighbour : graph.get(vertex)){
+                if(!vis[neighbour]){
                     q.add(neighbour);
+                    vis[neighbour] = true;
                 }
             }
         }
-
         return false;
     }
-
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-
         boolean[] vis = new boolean[n];
-
-        return bfs(n, edges, source, vis, destination);
+        return bfs(n , edges , source , destination , vis);
     }
 }
