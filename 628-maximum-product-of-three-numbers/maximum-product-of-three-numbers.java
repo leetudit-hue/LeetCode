45f60f7d038def543a6e4ad96710b9aff83cff66
leetudit-hue <leetudit@gmail.com>
2026-07-26 20:43:58 +0530

    Time: 3 ms (86.33%) | Memory: 47.2 MB (93.89%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -1,11 +1,36 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        int n = nums.length;
-        int ans1 = 1;
-        int ans2 = 1;
-        Arrays.sort(nums);
-        ans1 = (nums[n-1] * nums[n-2] * nums[n-3]);
-        ans2 = (nums[0] * nums[1] * nums[n-1]);
-        return Math.max(ans1 , ans2);
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        int third = Integer.MIN_VALUE;
+        int smin = Integer.MAX_VALUE;
+        int fmin = Integer.MAX_VALUE;
+        
+        for(int i =  0 ; i < nums.length ; i++){
+            if(nums[i] > first){
+                third = second;
+                second = first;
+                first = nums[i];
+            }
+            else if(nums[i] > second){
+                third = second;
+                second = nums[i];
+            }
+            else if(nums[i] > third){
+                third = nums[i];
+            }
+        }
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] < fmin){
+                smin = fmin;
+                fmin = nums[i];
+            }
+            else if(nums[i] < smin){
+                smin = nums[i];
+            }
+        }
+        int p1 = (first * second * third);
+        int p2 = (fmin * smin * first);
+        return Math.max(p1,p2);
     }
 }
