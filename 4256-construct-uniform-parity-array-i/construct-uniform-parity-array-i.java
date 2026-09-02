0142ac060fd34c08ad67ce458c4b48a10c2399ac
leetudit-hue <leetudit@gmail.com>
2026-09-02 23:34:21 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (87.60%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // int n = nums.length;
+        // int nOdd = 0;
+        // int nEve = 0;
+        // for(int i = 0 ; i < n ; i++){
+        //     if(nums1[i] % 2 == 0){
+        //         nEve ++;
+        //     }
+        //     else{
+        //         nOdd ++;,
+        //     }
+        // }
+        // if(nEve == n || nOdd == n) return true;
+        // else if(nEve > )
+        return true;
+    }
+}
